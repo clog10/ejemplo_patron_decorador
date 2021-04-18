7c82d438d51d1c239d8543c6e0ea1a73b2b55dfb
Carlos Loaeza <carlosloaeza100598@gmail.com>
2021-04-18 00:22:46 -0500

    Incompleto, Impresion Justificar Realizada, Corregir Vertical

--- src/decorar_texto/Justificar.java
+++ src/decorar_texto/Justificar.java
@@ -9,6 +9,31 @@ package decorar_texto;
  *
  * @author Carlos Loaeza
  */
-public class Justificar {
-    
+public class Justificar extends Decorador {
+
+    public Justificar(Texto tex) {
+        super(tex);
+        texto = super.getTexto();
+    }
+
+    @Override
+    public String getTexto() {
+        int tamañoRenglon = 80;
+        String t = new String();
+        int contador = 0;
+        for (int i = 0; i < texto.length(); i++) {
+            t += texto.charAt(i);
+            contador++;
+            if (contador == tamañoRenglon) {
+                t += "\n";
+                contador = 0;
+            }
+        }
+        return t;
+    }
+
+    @Override
+    public void setTexto(String t) {
+
+    }
 }
--- src/decorar_texto/PruebaDecorador.java
+++ src/decorar_texto/PruebaDecorador.java
@@ -10,7 +10,8 @@ package decorar_texto;
  * @author Carlos Loaeza
  */
 public class PruebaDecorador {
-    public static void main(String [] args){
+
+    public static void main(String[] args) {
         Texto textob = new TextoC("Ejemplo Decorador");
         //textob.setTexto("Ejemplo Decorador");
 //        System.out.println("Texto -> "+textob.getTexto());
@@ -19,17 +20,18 @@ public class PruebaDecorador {
 //        a.setAlinear('C');
 //        System.out.println(a.getTexto());
 
-        Texto ta = new Alinea(textob,'C');
+        Texto ta = new Alinea(textob, 'C');
         System.out.println(ta.getTexto());
-        
-        Texto tb = new Alinea(ta,'c');
+
+        Texto tb = new Alinea(ta, 'c');
         System.out.println(tb.getTexto());
-    
+
         //Lo que vamos a hacer
         //Texto tv =  new Vertical(new Alinear(new Texto("")));
-        
         Vertical v = new Vertical(new Alinear(new TextoC("Carlos")));
         System.out.println(v.getTexto());
-        
+
+        Justificar j = new Justificar(new Alinear(new TextoC("Los patrones de diseño son unas técnicas para resolver problemas comunes en el desarrollo de software y otros ámbitos referentes al diseño de interacción o interfaces. Un patrón de diseño resulta ser una solución a un problema de diseño. Para que una solución sea considerada un patrón debe poseer ciertas características. Una de ellas es que debe haber comprobado su efectividad resolviendo problemas similares en ocasiones anteriores. Otra es que debe ser reutilizable, lo que significa que es aplicable a diferentes problemas de diseño en distintas circunstancias.")));
+        System.out.println(j.getTexto());
     }
 }
